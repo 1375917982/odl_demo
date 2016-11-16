968541454ece3f828f7ec79293618796bb4fa8c1
hupeng <hupeng@imudges.com>
2016-11-16 13:40:10 +0800

    更新 主要更新在Mian.py 中，在tool.py中完成了http Basic Auth认证的简单实现

--- Main.py
+++ Main.py
@@ -4,20 +4,14 @@ class OdlUtil:
     url = ''
     def __init__(self, host, port):
         self.url = 'http://' + host + ':' + str(port)
-
-
     '''
     得到网络中的拓扑信息
     '''
-    def get_topology(self, container_name='default'):
+    def get_topology(self, container_name='default', username="admin", password="admin"):
         http = httplib2.Http()
-        http.add_credentials(name='admin', password='admin')
+        http.add_credentials(name=username, password=password)
         headers = {'Accept': 'application/json'}
         response, content = http.request(uri=self.url + '/controller/nb/v2/topology/' + str(container_name), headers=headers)
         return content.decode()
-
-
-
-
 odl = OdlUtil('127.0.0.1', '8080')
-print(odl.get_topology())
+print(odl.get_topology(username="admin", password="admin"))
